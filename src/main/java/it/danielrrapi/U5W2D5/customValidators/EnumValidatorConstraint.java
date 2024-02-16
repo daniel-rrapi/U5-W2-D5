0450d37f3338b9f4752cb0320d448c9b6422b432
Daniel Rrapi <uzo1209@gmail.com>
2024-02-16 15:06:50 +0100

    Added custom validator

---
+++ src/main/java/it/danielrrapi/U5W2D5/customValidators/EnumValidatorConstraint.java
@@ -0,0 +1,33 @@
+package it.danielrrapi.U5W2D5.customValidators;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class EnumValidatorConstraint implements ConstraintValidator<EnumValidator, String> {
+
+    List<String> valueList = null;
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        return valueList.contains(value.toUpperCase());
+    }
+
+    @Override
+    public void initialize(EnumValidator constraintAnnotation) {
+        valueList = new ArrayList<String>();
+        Class<? extends Enum<?>> enumClass = constraintAnnotation.enumClazz();
+
+        @SuppressWarnings("rawtypes")
+        Enum[] enumValArr = enumClass.getEnumConstants();
+
+        for (@SuppressWarnings("rawtypes") Enum enumVal : enumValArr) {
+            valueList.add(enumVal.toString().toUpperCase());
+        }
+    }
+}
--- src/main/java/it/danielrrapi/U5W2D5/payloads/NewDispositivoDTO.java
+++ src/main/java/it/danielrrapi/U5W2D5/payloads/NewDispositivoDTO.java
@@ -1,6 +1,17 @@
 package it.danielrrapi.U5W2D5.payloads;
 
-public record NewDispositivoDTO(
+import it.danielrrapi.U5W2D5.customValidators.EnumValidator;
+import it.danielrrapi.U5W2D5.entities.Dipendente;
+import it.danielrrapi.U5W2D5.enums.DispositivoStatus;
+import it.danielrrapi.U5W2D5.enums.DispositivoType;
+import jakarta.validation.constraints.NotNull;
 
-) {
+public record NewDispositivoDTO(
+        @EnumValidator(enumClazz = DispositivoType.class, message = "Enum errato")
+        String type,
+        @EnumValidator(enumClazz = DispositivoStatus.class, message = "Enum errato")
+        String status,
+        @NotNull
+        Dipendente dipendente
+        ) {
 }
